3ceb9b3d1d2eb2c002fb9fd6f52b7998f9a8a702
unknown <rr8603287@gmail.com>
2024-02-05 14:39:28 +0200

    weather_api

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:my_news_api/models/weather_model.dart';
-import 'package:my_news_api/pages/home_page.dart';
-import 'package:my_news_api/providers/weather_provider.dart';
+import 'package:weather_api/models/weather_model.dart';
+import 'package:weather_api/pages/home_page.dart';
+import 'package:weather_api/providers/weather_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
